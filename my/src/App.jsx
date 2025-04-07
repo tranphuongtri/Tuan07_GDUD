3706ab99c4898313b1915e2aa71a7d0286252783
tranphuongtri <gunnybboss@gmail.com>
2025-04-07 15:51:28 +0700

    GridLayoutTailwind

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -4,19 +4,8 @@ import Main from "./components/Main";
 function App() {
   return (
     <>
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex-1 bg-gray-50">
-        <Header />
-        <div className="p-6">
-          <div className="flex space-x-6">
-           
-          </div>
-          {/* <DetailedReport /> */}
-        </div>
-      </div>
-    </div>
-    {/* <Main></Main> */}
+
+      <Main></Main>
     </>
   )
 }
--- my/src/components/Main.jsx
+++ my/src/components/Main.jsx
@@ -2,24 +2,33 @@ import React from 'react';
 
 const Main = () => {
   return (
-    <div className="container grid grid-cols-1 md:grid-cols-4 grid-rows-[auto,1fr,auto] gap-5 bg-blue-500 p-5">
-      <div className="header col-span-4 text-center bg-white p-4 rounded-md">
-        <h2>DashBoard</h2>
-      </div>
-      <div className="menu bg-white p-4 rounded-md">
+    <div className="container grid grid-cols-1 md:grid-cols-3 grid-rows-[auto,1fr,auto] gap-5 bg-blue-500 p-5">
+      <div className="menu md:row-span-2 bg-white p-4 rounded-md ">
+        <button>Logo</button>
         <a href="#" className="block text-blue-500 hover:underline mb-2">Link</a>
         <a href="#" className="block text-blue-500 hover:underline mb-2">Link</a>
         <a href="#" className="block text-blue-500 hover:underline mb-2">Link</a>
       </div>
-      <div className="content bg-white p-4 rounded-md">
+      <div className="header md:col-span-3 text-center bg-white p-4 rounded-md">
+        <div className='flex justify-between items-center '><h2>DashBoard</h2>
+          <div className='flex items-center'>
+            <input type="text" name="" id="" className='border border-gray-300 px-2 py-1 rounded' />
+            <button className='mx-3'>Icon 1</button>
+            <button className='mx-3'>Icon 2</button>
+            <button className='mx-3'>Avatar</button></div></div>
+      </div>
+
+      <div className="content md:col-span-3 bg-white p-4 rounded-md h-108">
         <h3>Overview</h3>
-        <p></p>
+        <p>Text 1</p>
+        <h3>Detailed Report</h3>
+        <p>Text 2 </p>
       </div>
-      <div className="content bg-white p-4 rounded-md">
+      {/* <div className="content bg-white p-4 rounded-md">
         <h3>Detailed Report</h3>
         <p></p>
-      </div>
-      <div className="footer col-span-4 text-center bg-white p-4 rounded-md">
+      </div> */}
+      <div className="footer md:col-span-4 text-center bg-white p-4 rounded-md">
         <h4>Footer</h4>
       </div>
     </div>
